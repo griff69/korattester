ebce622d2e5f5ece530b09c3341758a60366850e
scullz78 <scullz78@448099bd-7d26-0410-a59f-638b6ef08c89>
2007-03-26 23:35:26 +0000

    this version is before generics were implemented.  Generics ended up breaking it, so I'm marking this as the last "working" version.  Hopefully I'll get to leverage generics to a point still.

---
+++ tags/20070326_pre-generics/src/com/scully/korat/test/SearchTree_remove.java
@@ -0,0 +1,107 @@
+package com.scully.korat.test;
+
+
+import com.scully.korat.KoratClient;
+import com.scully.korat.map.BeanXmlMapper;
+import com.scully.korat.map.StateSpaceBuilder;
+import com.scully.korat.map.TestStateSpaceDTO;
+
+public class SearchTree_remove {
+    
+//	public static Finitization finSearchTree (
+//			int NUM_Node,
+//			int MIN_size,
+//			int MAX_size,
+//			int MIN_Node_value,
+//			int MAX_Node_value) {
+//	    Finitization f = new Finitization(SearchTree.class);
+//	    ObjSet nodes = f.createObjects("com.scully.korat.test.SearchTree$Node", NUM_Node);
+//		nodes.add(null);
+//		try
+//        {
+//			f.set(SearchTree.class.getDeclaredField("root"), nodes);
+//			f.set(SearchTree.class.getDeclaredField("size"), new IntSet(MIN_size, MAX_size));
+//			f.set(SearchTree.Node.class.getDeclaredField("right"), nodes);
+//			f.set(SearchTree.Node.class.getDeclaredField("left"), nodes);
+//            f.set(SearchTree.Node.class.getDeclaredField("value"), new IntSet(MIN_Node_value, MAX_Node_value));
+//        }
+//        catch (SecurityException e)
+//        {
+//            e.printStackTrace();
+//        }
+//        catch (NoSuchFieldException e)
+//        {
+//            e.printStackTrace();
+//        }
+//		return f;
+//	}
+    
+    public static TestStateSpaceDTO createFiniteStateSpace (
+            int NUM_Node,
+            int MIN_size,
+            int MAX_size,
+            int MIN_Node_value,
+            int MAX_Node_value) {
+        
+        StateSpaceBuilder stateSpace = new StateSpaceBuilder();
+
+        // set the root
+        stateSpace.setRootClass("com.scully.korat.test.SearchTree");
+
+        // ==> create StateObjects
+
+        // Node
+        stateSpace.addStateObject("com.scully.korat.test.SearchTree$Node", NUM_Node, true);
+
+        // ==> create StateFields
+
+        // SearchTree
+        // root
+        stateSpace.addStateField("root", "com.scully.korat.test.SearchTree$Node", "com.scully.korat.test.SearchTree");
+        
+        // size
+        stateSpace.addStateField("size", int.class.getName(), "com.scully.korat.test.SearchTree", MIN_size, MAX_size);
+
+        // SearchTree$Node
+        // left
+        stateSpace.addStateField("left", "com.scully.korat.test.SearchTree$Node", "com.scully.korat.test.SearchTree$Node");
+        
+        // right
+        stateSpace.addStateField("right", "com.scully.korat.test.SearchTree$Node", "com.scully.korat.test.SearchTree$Node");
+        
+        // value
+        stateSpace.addStateField("value", int.class.getName(), "com.scully.korat.test.SearchTree$Node", MIN_Node_value, MAX_Node_value);
+        
+        return stateSpace.getStateSpace();
+    }
+
+	public static TestStateSpaceDTO createFiniteStateSpace(int scope) {
+	    // TODO: Verify helper scope
+		return createFiniteStateSpace(
+			scope, 0, scope, 1, scope);
+	}
+	
+	public static void main(String[] args) {
+	    // TODO: Define Finitization Scope
+        TestStateSpaceDTO testStateSpace = createFiniteStateSpace(3);
+//	    Finitization f = finSearchTree(3, 0, 3, 1, 3);
+	    
+	    // remove method signature
+	    String testMethod = "remove";
+	    Class[] paramTypes = new Class[] { int.class };
+	    // TODO: Define test parameters for inputs
+	    Object[] paramValues = new Object[] { new Integer(1) };
+		
+	    // TODO: Choose Korat feature execution
+	    KoratClient.setIsoMorphismBreaking(true);
+	    KoratClient.populateTestCandidates(testStateSpace);
+        System.out.println(BeanXmlMapper.beanToXml(testStateSpace));
+	    KoratClient.setPruning(false);
+        KoratClient.testMethod(testStateSpace, testMethod, paramTypes, paramValues);
+//	    KoratClient.getFiniteStateSpace(stateSpace, testMethod, paramTypes, paramValues);
+//		String serializePath = "c:/ut/korat-cvs/scope-4-noniso";
+//	    KoratClient.serializeInputs(f, serializePath);
+//		KoratClient.testSerializedObjects(f, testMethod, paramTypes, paramValues, serializePath);
+	}
+    
+}
---
+++ tags/20070326_pre-generics/test/com/scully/korat/KoratTestSuite.java
@@ -0,0 +1,30 @@
+package com.scully.korat;
+
+import com.scully.korat.finitization.FinitizationTest;
+import com.scully.korat.instrument.InstrumenterTest;
+import com.scully.korat.map.BeanXmlMapperTest;
+import com.scully.korat.map.StateSpaceBeanTest;
+import com.scully.korat.test.SearchTreeInstrumentTest;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class KoratTestSuite
+{
+
+    public static Test suite()
+    {
+        TestSuite suite = new TestSuite("Test for com.scully.korat");
+        //$JUnit-BEGIN$
+//        suite.addTestSuite(KoratSearchTest.class);
+        suite.addTestSuite(CandidateStateTest.class);
+        suite.addTestSuite(FinitizationTest.class);
+        suite.addTestSuite(StateSpaceBeanTest.class);
+        suite.addTestSuite(BeanXmlMapperTest.class);
+        suite.addTestSuite(InstrumenterTest.class);
+        suite.addTestSuite(SearchTreeInstrumentTest.class);
+        //$JUnit-END$
+        return suite;
+    }
+
+}
